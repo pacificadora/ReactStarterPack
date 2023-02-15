c33be5b0dae5f9cc7033337b63e3597c7d4e8ea4
pacificadora <singhalaman198@gmail.com>
2023-02-15 14:55:53 +0530

    second commit

---
+++ src/Components/Greet.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Greet() {
+  return <h1>Hello Aman</h1>;
+}
+
+// const Greet1 = () => {
+//   return <h1>Hello Singhal</h1>;
+// };
+
+export  {Greet };
---
+++ src/Components/Welcome.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Component } from "react";
+
+export class Welcome extends Component{
+    render(){
+        return <h1>Class Component</h1>
+    }
+}
